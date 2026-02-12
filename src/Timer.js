24a6831668c41c3e998938aa419a55eea4a04740
Brian <imbrian01wilson@gmail.com>
2026-02-12 11:12:29 +0330

    set timer and some style change

--- src/Timer.js
+++ src/Timer.js
@@ -7,23 +7,70 @@ class Timer extends Component {
   constructor() {
     super();
     this.state = {
-      time: new Date().toLocaleTimeString(),
+      second: 0,
+      minute: 0,
+      hour: 0,
+      isStart: false,
     };
   }
 
-  componentDidMount() {
+  startInterval = () => {
+    this.setState({
+      isStart: true,
+    });
+
     interval = setInterval(() => {
       this.setState({
-        time: new Date().toLocaleTimeString(),
+        second: this.state.second + 1,
       });
+
+      if (this.state.second == 60) {
+        this.setState({
+          second: 0,
+          minute: this.state.minute + 1,
+        });
+      }
+
+      if (this.state.minute == 60) {
+        this.setState({
+          minute: 0,
+          hour: this.state.hour + 1,
+        });
+      }
     }, 1000);
-  }
+  };
+
+  stopInterval = () => {
+    this.setState({
+      isStart: false,
+    });
+    clearInterval(interval);
+  };
+
+  resetInterval = () => {
+    this.stopInterval();
+    this.setState({
+      second: 0,
+      minute: 0,
+      hour: 0,
+    });
+  };
 
   render() {
+    let h = this.state.hour;
+    let m = this.state.minute;
+    let s = this.state.second;
     return (
       <>
-        <h1 className="timer">{this.state.time}</h1>
-        <button className="btn" onClick={this.props.changeTitle}>change title</button>
+        <h1 className="timer">{`${h > 9 ? h : "0" + h} : ${m > 9 ? m : "0" + m} : ${s > 9 ? s : "0" + s}`}</h1>
+        <div className="buttons">
+          <button className="btn" onClick={this.props.changeTitle}>
+          change title
+        </button>
+        <button className="btn-start" onClick={this.startInterval}>Start</button>
+        <button className="btn-stop" onClick={this.stopInterval}>Stop</button>
+        <button className="btn-reset" onClick={this.resetInterval}>Reset</button>
+        </div>
       </>
     );
   }
